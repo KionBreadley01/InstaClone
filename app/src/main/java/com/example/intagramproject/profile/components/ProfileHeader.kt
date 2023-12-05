4ddc7926676dff92da4c219e58bd0426d5e9741b
kionBreadley01 <juansanchezjuan125@gmail.com>
2023-12-05 14:14:30 -0600

    avances del clone de instagram V.2

---
+++ app/src/main/java/com/example/intagramproject/profile/components/ProfileHeader.kt
@@ -0,0 +1,21 @@
+package com.example.intagramproject.profile.components
+
+import androidx.compose.foundation.layout.Row
+import
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ProfileHeader(){
+    Row(modifier = Modifier.fillMaxWindth()){
+
+    }
+
+}
+@Preview(showBackground = true )
+@Composable
+fun ProfileHeaderPreview(){
+    ProfileHeader()
+}
+
